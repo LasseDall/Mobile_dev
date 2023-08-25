437669c05f72ba21392376002a46e32dda2d4bca
LasseDall <lass794rrstud.kea.dk>
2023-08-25 15:12:41 +0200

    Nestle

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source = {require("./assets/rainbow.jpg")} resizeMode='cover' style={styles.backgroundImage}>
+      <Image source = {require("./assets/nestle.jpg")} style={{width: 300}}/>
+      <Text style={styles.text}>Good Food, Good Life</Text>
       <StatusBar style="auto" />
+      <Button title='Donate' onPress={() => setCount(count+1)}></Button>
+      <Text style={styles.text}>{count}</Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +20,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 35,
+    color: 'pink',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
